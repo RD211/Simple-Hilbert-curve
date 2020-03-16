eb4171308d98965bd9b5602b86346793947cdf7e
RD211 <rdyt211@gmail.com>
2020-03-16 16:27:30 +0200

    Add project files.

---
+++ Hilbert/Form1.Designer.cs
@@ -0,0 +1,102 @@
+﻿namespace Hilbert
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pbox_view = new System.Windows.Forms.PictureBox();
+            this.btn_generate = new System.Windows.Forms.Button();
+            this.num_order = new System.Windows.Forms.NumericUpDown();
+            ((System.ComponentModel.ISupportInitialize)(this.pbox_view)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.num_order)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pbox_view
+            // 
+            this.pbox_view.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.pbox_view.Location = new System.Drawing.Point(0, 0);
+            this.pbox_view.Name = "pbox_view";
+            this.pbox_view.Size = new System.Drawing.Size(512, 512);
+            this.pbox_view.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pbox_view.TabIndex = 0;
+            this.pbox_view.TabStop = false;
+            // 
+            // btn_generate
+            // 
+            this.btn_generate.Location = new System.Drawing.Point(437, 29);
+            this.btn_generate.Name = "btn_generate";
+            this.btn_generate.Size = new System.Drawing.Size(75, 23);
+            this.btn_generate.TabIndex = 1;
+            this.btn_generate.Text = "Generate";
+            this.btn_generate.UseVisualStyleBackColor = true;
+            this.btn_generate.Click += new System.EventHandler(this.Btn_generate_Click);
+            // 
+            // num_order
+            // 
+            this.num_order.Location = new System.Drawing.Point(441, 3);
+            this.num_order.Minimum = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            this.num_order.Name = "num_order";
+            this.num_order.Size = new System.Drawing.Size(71, 20);
+            this.num_order.TabIndex = 2;
+            this.num_order.Value = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(517, 516);
+            this.Controls.Add(this.num_order);
+            this.Controls.Add(this.btn_generate);
+            this.Controls.Add(this.pbox_view);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.SizeChanged += new System.EventHandler(this.Form1_SizeChanged);
+            ((System.ComponentModel.ISupportInitialize)(this.pbox_view)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.num_order)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pbox_view;
+        private System.Windows.Forms.Button btn_generate;
+        private System.Windows.Forms.NumericUpDown num_order;
+    }
+}
+
---
+++ Hilbert/Form1.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Drawing.Drawing2D;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Hilbert
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        static PointF RotatePoint(PointF pointToRotate, PointF centerPoint, double angleInDegrees)
+        {
+            double angleInRadians = angleInDegrees * (Math.PI / 180);
+            double cosTheta = Math.Cos(angleInRadians);
+            double sinTheta = Math.Sin(angleInRadians);
+            return new Point
+            {
+                X =
+                    (int)
+                    (cosTheta * (pointToRotate.X - centerPoint.X) -
+                    sinTheta * (pointToRotate.Y - centerPoint.Y) + centerPoint.X),
+                Y =
+                    (int)
+                    (sinTheta * (pointToRotate.X - centerPoint.X) +
+                    cosTheta * (pointToRotate.Y - centerPoint.Y) + centerPoint.Y)
+            };
+        }
+        public List<PointF> GenerateHilbert(int order, float width, float height, float xOff, float yOff)
+        {
+            if (order == 1)
+            {
+                return new List<PointF>() { new PointF(xOff + 0 + width/4, yOff + 0 + width/4), new PointF(xOff + 0 + width/4, yOff + height - height/4), new PointF(xOff + width - width/4, yOff + height - height/4), new PointF(xOff + width - width/4, yOff + 0 + height/4) };
+            }
+            else
+            {
+                var leftUp = GenerateHilbert(order - 1, width / 2, height / 2, xOff, yOff);
+                for(int i =0;i<leftUp.Count;i++)
+                {
+                    leftUp[i] = RotatePoint(leftUp[i], new PointF(xOff + (float)width / 4, yOff + (float)height / 4), -90);
+                }
+                leftUp.Reverse();
+                var rightUp = GenerateHilbert(order - 1, width / 2, height / 2, xOff + width / 2, yOff);
+                for (int i = 0; i < rightUp.Count; i++)
+                {
+                    rightUp[i] = RotatePoint(rightUp[i], new PointF(xOff + (width / 4)*3, yOff + height / 4), 90);
+                }
+                rightUp.Reverse();
+                var leftDown = GenerateHilbert(order - 1, width / 2, height / 2, xOff, yOff + height / 2);
+                var rightDown = GenerateHilbert(order - 1, width / 2, height / 2, xOff + width / 2, yOff + height / 2);
+                List<PointF> r = new List<PointF>();
+                
+                return r.Concat(leftUp).Concat(leftDown).Concat(rightDown).Concat(rightUp).ToList();
+            }
+        }
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Btn_generate_Click(object sender, EventArgs e)
+        {
+            Bitmap bmp = new Bitmap(1023, 1023);
+            Graphics g = Graphics.FromImage(bmp);
+            Pen pn = new Pen(Color.BlueViolet, 2f);
+            g.DrawLines(pn, GenerateHilbert((int)num_order.Value, bmp.Width, bmp.Height, 0, 0).ToArray());
+            this.pbox_view.Image = bmp;
+        }
+
+        private void Form1_SizeChanged(object sender, EventArgs e)
+        {
+            this.Width = this.Height;
+        }
+    }
+}
